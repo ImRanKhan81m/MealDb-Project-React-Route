3e47282a30961060ea44027b1109283703589086
Imran Hossen <freelancerimran81@gmail.com>
2022-03-30 20:05:19 +0600

    header style done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
     return (
@@ -11,19 +10,18 @@ const Header = () => {
                     <Navbar.Brand href="#home"><h2>Food Gallery</h2></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <CustomLink to={'/home'}>Home</CustomLink>
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <NavDropdown title="Category" id="basic-nav-dropdown">
+                        <Nav className="ms-auto items">
+                            <Nav.Link className='item' href="#home">Home</Nav.Link>
+                            <NavDropdown className='item' title="Category" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Grocery</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Sweets</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Drinks</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Other's Product</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#cart">Cart</Nav.Link>
+                            <Nav.Link className='item' href="#contact">Contact</Nav.Link>
+                            <Nav.Link className='item' href="#about">About</Nav.Link>
+                            <Nav.Link className='item' href="#cart">Cart</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
