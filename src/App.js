a324829b6d57c1e92bc5617e78d21e18ce07b908
Imran Hossen <freelancerimran81@gmail.com>
2022-03-31 18:45:35 +0600

    update

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import Shop from './Components/Shop/Shop';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/shop' element={<Shop/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/cart' element={<Cart/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </div>
   );
