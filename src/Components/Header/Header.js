aab0250c345c2a7627c322f533fe81492c076f76
Imran Hossen <freelancerimran81@gmail.com>
2022-03-31 10:36:32 +0600

    header section style updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
@@ -7,21 +9,15 @@ const Header = () => {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home"><h2>Food Gallery</h2></Navbar.Brand>
+                    <Navbar.Brand><Link to={'/'}><h2>Food Gallery</h2></Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto items">
-                            <Nav.Link className='item' href="#home">Home</Nav.Link>
-                            <NavDropdown className='item' title="Category" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Shop</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Sweets</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Drinks</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Other's Product</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link className='item' href="#contact">Contact</Nav.Link>
-                            <Nav.Link className='item' href="#about">About</Nav.Link>
-                            <Nav.Link className='item' href="#cart">Cart</Nav.Link>
+                            <CustomLink className='item' to={'/'}>Home</CustomLink>
+                            <CustomLink className='item' to={'/shop'}>Shop</CustomLink>
+                            <CustomLink className='item' to={'/contact'}>Contact</CustomLink>
+                            <CustomLink className='item' to={'/about'}>About</CustomLink>
+                            <CustomLink className='item' to={'/cart'}>Cart</CustomLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
