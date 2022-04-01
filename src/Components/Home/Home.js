099ed5795f38169011e343efd7267130f9398e26
Imran Hossen <freelancerimran81@gmail.com>
2022-04-01 23:44:02 +0600

    update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,12 +16,12 @@ const Home = () => {
                                 <p><span>Our Special Dish</span></p>
                                 <h1>Fried Chicken</h1>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem similique dicta praesentium asperiores.</p>
-                                <button onClick={()=>navigate('/shop')}>Order Now</button>
+                                <button onClick={() => navigate('/shop')}>Order Now</button>
                             </div>
                         </Col>
                         <Col lg='6' md='6' className='img-container'>
                             <div>
-                                <img src={image} alt=""/>
+                                <img src={image} alt="" />
                             </div>
                         </Col>
                     </Row>
