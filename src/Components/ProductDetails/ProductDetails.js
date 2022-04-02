438e9a1e0b5e7464c0778956b7a3bfdeea476ed6
Imran Hossen <freelancerimran81@gmail.com>
2022-04-02 20:02:07 +0600

    updated

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -5,7 +5,8 @@ import './ProductDetails.css'
 
 const ProductDetails = () => {
     const { id } = useParams();
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState({});
+    const{strMeal,strArea,strInstructions,strTags,strYoutube,strSource,strIngredient1,strIngredient2,strIngredient3,strIngredient4,strIngredient5,strIngredient6,strIngredient7,strIngredient8,strIngredient9,strIngredient10,strIngredient11,strIngredient12,strIngredient13  } = product;
 
     useEffect(() => {
         const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
@@ -23,7 +24,11 @@ const ProductDetails = () => {
                             <img src={product.strMealThumb} alt="" width={'80%'}/>
                         </div>
                     </Col>
-                    <Col lg='6'><h1>Imran</h1></Col>
+                    <Col lg='6'>
+                        <div className="product-item-details">
+
+                        </div>
+                    </Col>
                 </Row>
             </div>
         </div>
