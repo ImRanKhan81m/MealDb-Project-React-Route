8eac596c9d0b548bb426c2b4a882bbc680e24914
Imran Hossen <freelancerimran81@gmail.com>
2022-04-02 16:13:37 +0600

    button style

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import ShopDetails from '../ShopDetails/ShopDetails';
+import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
--- src/Components/ShopDetails/ShopDetails.js
+++ src/Components/ShopDetails/ShopDetails.js
@@ -1,11 +1,30 @@
+
 import React from 'react';
+import { Button, Card, CardGroup } from 'react-bootstrap';
+import './ShopDetails.css'
 
 const ShopDetails = (props) => {
-    const {strMealThumb,strMeal, strInstructions } = props.product
+    const { strMealThumb, strMeal, strInstructions } = props.product
     return (
         <div>
-            <div>
-                <h1>Name:{strMeal}</h1>
+            <div className='shopDetails'>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src={strMealThumb} />
+                        <Card.Body>
+                            <Card.Title>{strMeal}</Card.Title>
+                            <div className='cards'>
+                                <Card.Text>
+                                    {strInstructions.slice(0, 100)}
+                                </Card.Text>
+                            </div>
+                            <div className='btn'>
+                             <div><Button variant="primary">See Details</Button></div>
+                            <div><Button variant="primary">Add To Cart</Button></div>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
             </div>
         </div>
     );
