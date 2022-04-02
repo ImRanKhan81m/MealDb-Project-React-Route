3f79d28ca9fe0d660de868feaad0be01e8071eda
Imran Hossen <freelancerimran81@gmail.com>
2022-04-02 15:31:12 +0600

    search product show

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -10,19 +10,23 @@ const Shop = () => {
         setSearchText(food.target.value);
     }
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=cake')
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        fetch(url)
             .then(res => res.json())
             .then(data => setProducts(data.meals))
-    }, [])
+    }, [searchText])
     return (
         <div className="container">
-            <div className="search-container">
+            <div className="search-container text-center my-5 py-5">
                 <h2>Please search your favorite food</h2>
-                <input onChange={searchFood} type="text" placeholder='search your favorite food name' /> <button>Search</button>
+                <input className='w-25' onChange={searchFood} type="text" placeholder='search your favorite food name' /> <button>Search</button>
             </div>
             <div className="product-result-container">
                 {
-                    products.map(product => <ShopDetails product={product}></ShopDetails>)
+                    products.map(product => <ShopDetails 
+                        product={product}
+                        key={product.MealId}
+                        ></ShopDetails>)
                 }
             </div>
         </div>
