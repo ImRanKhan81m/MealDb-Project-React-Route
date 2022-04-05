9929365200c0e39ee583a364e6ebb2e1b623e4b2
Imran Hossen <freelancerimran81@gmail.com>
2022-04-05 21:41:42 +0600

    update

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
-            <p>Sorry we cann't find this page</p>
+            <p>Sorry we can't find this page</p>
         </div>
     );
 };
